96aa8f6e37a8cf9bc636d80a8817402f3f80a761
Can Kolho <can.kolho@hotmail.com>
2024-01-13 20:15:54 +0200

    Updates

--- assets/Home-Q7FhXO5_.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as e,j as t,M as i,T as s}from"./index-tG5Mo7eV.js";import{u as a}from"./useMediaQuery-0PvNblOQ.js";const n=r.lazy(()=>e(()=>import("./ImageGallery-lPrAEjm1.js"),__vite__mapDeps([0,1,2,3,4,5,6]))),m=r.lazy(()=>e(()=>import("./Footer-S01DnyUn.js"),__vite__mapDeps([7,1,2,8]))),x=()=>{const o=a("(max-width: 990px)");return t.jsxs(t.Fragment,{children:[t.jsx(i,{index:1,direction:"down",children:t.jsx(s,{variant:o?"h3":"h2",align:"center",fontWeight:700,mb:10,mt:15,children:"RONJA KOLHO"})}),t.jsx(n,{}),t.jsx(m,{})]})};export{x as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/ImageGallery-lPrAEjm1.js","assets/index-tG5Mo7eV.js","assets/index-aayiM9r2.css","assets/MyImage-rVpcWLT3.js","assets/MyImage-pao4Rfbo.css","assets/useMediaQuery-0PvNblOQ.js","assets/ImageGallery-Y5oB72m1.css","assets/Footer-S01DnyUn.js","assets/Footer-5_INJNFm.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
