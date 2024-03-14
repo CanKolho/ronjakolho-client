2b9a7014cc4fee83cc0d3b0bc4538180456a2858
Can Kolho <can.kolho@hotmail.com>
2024-03-14 18:39:08 +0200

    Updates

---
+++ assets/Home-_HNWND0n.js
@@ -0,0 +1,7 @@
+import{r as t,_ as o,j as r}from"./index-LzFP3sjf.js";const e=t.lazy(()=>o(()=>import("./ImageGallery-9rtmWRGc.js"),__vite__mapDeps([0,1,2,3,4,5]))),s=t.lazy(()=>o(()=>import("./Footer-LnT6zC4U.js"),__vite__mapDeps([6,1,2,7]))),a=()=>r.jsxs(r.Fragment,{children:[r.jsx(e,{}),r.jsx(s,{})]});export{a as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/ImageGallery-9rtmWRGc.js","assets/index-LzFP3sjf.js","assets/index-aayiM9r2.css","assets/MyImage-QJHRcsyy.js","assets/MyImage-pao4Rfbo.css","assets/ImageGallery-Wwv3Rpqe.css","assets/Footer-LnT6zC4U.js","assets/Footer-5_INJNFm.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- assets/Home-xjpZQsKI.js
+++
@@ -1,7 +0,0 @@
-import{r,_ as e,j as t,M as i,T as s}from"./index-VLiRddaK.js";import{u as a}from"./useMediaQuery-HeiH8MND.js";const n=r.lazy(()=>e(()=>import("./ImageGallery-nvb-vxzM.js"),__vite__mapDeps([0,1,2,3,4,5,6]))),m=r.lazy(()=>e(()=>import("./Footer-WIawa43k.js"),__vite__mapDeps([7,1,2,8]))),x=()=>{const o=a("(max-width: 990px)");return t.jsxs(t.Fragment,{children:[t.jsx(i,{index:1,direction:"down",children:t.jsx(s,{variant:o?"h3":"h2",align:"center",fontWeight:700,mb:10,mt:15,children:"RONJA KOLHO"})}),t.jsx(n,{}),t.jsx(m,{})]})};export{x as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/ImageGallery-nvb-vxzM.js","assets/index-VLiRddaK.js","assets/index-aayiM9r2.css","assets/MyImage-W-jDzGYM.js","assets/MyImage-pao4Rfbo.css","assets/useMediaQuery-HeiH8MND.js","assets/ImageGallery-Y5oB72m1.css","assets/Footer-WIawa43k.js","assets/Footer-5_INJNFm.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
